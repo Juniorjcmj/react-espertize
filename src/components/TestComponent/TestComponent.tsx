0704bb0cc7f6452cc0811dceccebc6377a943f21
José Carlos <jotacfs2010@hotmail.com>
2022-01-25 01:13:15 -0300

    Create TestComponent and move App to the components directory

---
+++ src/components/TestComponent/TestComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './TestComponent.css';
+
+
+const TestComponent = ( ) => (
+
+    <div className="TestComponent">'Marinha do brasil'</div>
+)
+
+
+export default TestComponent;
