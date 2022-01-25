dc54e1297d3a92a85760531ecdb3d3fd4d0bf3f4
José Carlos <jotacfs2010@hotmail.com>
2022-01-25 13:06:33 -0300

    Organizando os componentes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import Button from '../Button';
+import Button from '../../shared/Button';
 import Header from '../Header';
 import './App.css';
+import Container from '../../shared/Container/Container';
 
 const TestComponent = () =>{
   return <img width={16} src='http://1.bp.blogspot.com/-EXLt4mka7Qs/U-DiNSKETSI/AAAAAAAAGqI/lfw8PYnoNuk/s1600/Bot%C3%A3o-Desliga-em-png-queroiamgem-Cei%C3%A7a-Crispim+(1).png' alt='clicar'></img>
@@ -12,16 +13,16 @@ function App() {
   return (
     <div className="App">
      <Header title = 'Sistema Gestão' />    
-      <div className = 'Container'  >
+      <Container >
        <Button
        onClick={()=> window.alert('Clicou no botão')}
        appendIcon={<TestComponent />}
        >
          Potato
        </Button>
+     </Container>
       </div>
        
-    </div>
   );
 }
 
