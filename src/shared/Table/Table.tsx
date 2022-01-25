bc38d966b1f30c9fc727172c9842ab4c13da6bed
José Carlos <jotacfs2010@hotmail.com>
2022-01-25 18:01:10 -0300

    mock para tabela e inserindo informações dinâmicas

--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import './Table.scss'
+import Produts from './Table.mockdata'
+import Products from './Table.mockdata'
+
+
+const headers = [
+    {key: 'name', value: 'Product'},
+    {key: 'price', value: 'Price'},
+    {key: 'stock', value: 'Available Stock'}
+]
+
 declare interface TableProps{
 
 }
@@ -7,19 +17,24 @@ const Table: React.FC<TableProps> = (props)=> {
      return <table className='AppTable'>
        <thead>
            <tr>
-               <th>Empresa</th>
-               <th>Data Vencimento</th>
-               <th>Valor da Duplicata</th>
-               <th>Fornecedor</th>
-               <th>Número do Doc</th>
+               {
+                headers.map (header => <th key={header.key}>
+                    {header.value}
+                   </th>
+                   )
+                   }  
+               
            </tr>
        </thead>
-       <tbody>
-           <td>CN Muzema</td>
-           <td>30/01/2022</td>
-           <td>R$ 280,00</td>
-           <td>Amanco</td>
-           <td>7777725369</td>
+       <tbody>           
+           {
+              Products.map((product => <tr key={product.id}>
+                  <td>{product.name}</td>
+                  <td>{product.price}</td>
+                  <td>{product.stock}</td>
+              </tr>))
+           }
+           
        </tbody>
 
      </table>
