80ec40511d84d89d2ef893565c795f90b32ef916
José Carlos <jotacfs2010@hotmail.com>
2022-01-25 20:37:45 -0300

    Ciando formulario

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,9 @@ import './App.css';
 import Container from '../../shared/Container/Container';
 import Table, { TableHeader } from '../../shared/Table';
 import Products from '../../shared/Table/Table.mockdata';
+import Form from '../../shared/Form';
+import Input from '../../shared/Input';
+import Button from '../../shared/Button';
 
 const headers: TableHeader[] = [
   {key: 'id', value: 'ID'},
@@ -20,6 +23,31 @@ function App() {
            headers={headers}
            data={Products}
            />
+           <Form 
+           title='Formulário de produtos'
+           onSubmit={console.log}
+           >
+             <Input 
+                label='Name'
+                placeholder='E.g: Cookie'
+             />
+             <Input 
+                label='Price'
+                type= "number"
+                step= "0.01"
+                min="0"
+                placeholder='E.g: 1.25'
+             />
+             <Input 
+                label='Estoque'
+                type= "number"
+                min="0"
+                placeholder='E.g: Cookie'
+             />
+             <Button>
+                Submit
+             </Button>
+           </Form>
      </Container>
       </div>       
   );
