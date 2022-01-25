2aa0fb532eec8162b0c7a5fe6844810e0480dee7
José Carlos <jotacfs2010@hotmail.com>
2022-01-25 19:40:32 -0300

    fix table generica

--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -3,41 +3,93 @@ import './Table.scss'
 import Produts from './Table.mockdata'
 import Products from './Table.mockdata'
 
-
-const headers = [
+const headers: TableHeader[] = [
+    {key: 'id', value: 'ID'},
     {key: 'name', value: 'Product'},
-    {key: 'price', value: 'Price'},
-    {key: 'stock', value: 'Available Stock'}
+    {key: 'price', value: 'Price',right: true },
+    {key: 'stock', value: 'Available Stock', right: true}
 ]
 
-declare interface TableProps{
-
+declare interface TableHeader{
+ key: string
+ value: string
+ right?: boolean
 }
-const Table: React.FC<TableProps> = (props)=> {
-     return <table className='AppTable'>
-       <thead>
-           <tr>
-               {
-                headers.map (header => <th key={header.key}>
-                    {header.value}
-                   </th>
-                   )
-                   }  
-               
-           </tr>
-       </thead>
-       <tbody>           
-           {
-              Products.map((product => <tr key={product.id}>
-                  <td>{product.name}</td>
-                  <td>{product.price}</td>
-                  <td>{product.stock}</td>
-              </tr>))
-           }
-           
-       </tbody>
+type IndexedHeaders = {
+    [key: string]: TableHeader
+}
+type OrganizedItem = {
+    [key: string]: any
+}
 
-     </table>
+function organizeData (data: any[], headers: TableHeader[]):
+  [OrganizedItem[], IndexedHeaders] {
+  const indexedHeaders: IndexedHeaders = {}
+
+  headers.forEach(header => {
+    indexedHeaders[header.key] = {
+      ...header
+    }
+  })
+    const headerKeysInOrder = Object.keys(indexedHeaders)
+
+    const organizedData = data.map(item => {
+        const organizedItem: OrganizedItem = {}
+    
+        headerKeysInOrder.forEach(key => {
+          organizedItem[key] = item[key]
+        })
+    
+        organizedItem.$original = item
+    
+        return organizedItem
+      })
+    
+      return [organizedData, indexedHeaders]
+    }
+    
+
+const Table = ()=> {
+    const [organizedData, indexedHeaders] = organizeData(Products, headers)
+
+     return <table className="AppTable">
+     <thead>
+       <tr>
+         {
+           headers.map(header =>
+             <th
+               className={header.right ? 'right' : ''}
+               key={header.key}
+             >
+               {header.value}
+             </th>
+           )
+         }
+       </tr>
+     </thead>
+     <tbody>
+       {
+         organizedData.map((row, i) => {
+           return <tr key={i}>
+             {
+               Object
+                 .keys(row)
+                 .map((item, i) =>
+                   item !== '$original'
+                     ? <td
+                         key={row.$original.id + i}
+                         className={indexedHeaders[item].right ? 'right' : ''}
+                       >
+                         { row[item] }
+                       </td>
+                     : null
+                 )
+             }
+           </tr>
+         })
+       }
+     </tbody>
+   </table>
 }
 
 export default Table;
