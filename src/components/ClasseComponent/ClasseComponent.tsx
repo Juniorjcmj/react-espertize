4a7ad40a65f60a60a09d21700fcec9938717f989
José Carlos <jotacfs2010@hotmail.com>
2022-01-25 02:04:09 -0300

    Ciclo de vida

--- src/components/ClasseComponent/ClasseComponent.tsx
+++ src/components/ClasseComponent/ClasseComponent.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
 class ClassComponent extends React.Component<{name: string}>{
+    constructor(props: any){
+        super(props)
+        console.log('01 no ciclo de vida Construtor')
+    }
     
     state ={
         name: 'Olá mundo Java'
     };
 
+    componentDidMount(){
+        console.log('03 ciclo de vida DidMount')
+    }
+    componentDidUpdate(){
+        console.log('04 no ciclo de vida mas só executa se houver alteração no component, DidUpdate')
+    }
+
     render()  {
         //return <div>{this.props.name}. eu sou um component baseado em classe</div>
-
+       console.log('02 no ciclo de vida render')
         return <div>
             <p>name: {this.state.name}</p>
             <button onClick={() =>
